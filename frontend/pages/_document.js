2c7804349121f794721a0b22b5f0fc982201e00c
Angel <i02naroa@uco.es>
2026-06-10 14:14:21 +0200

    feat(frontend): add favicons and web manifest to Document head

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/favicon_io/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
+      </Head>
       <body>
         <script
           dangerouslySetInnerHTML={{
